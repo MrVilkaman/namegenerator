75892052d27927b3c191e38ea71f38849d68d060
Zahar Zolotarev <zahar.zolotarev@gmail.com>
2016-03-12 12:07:55 +0300

    init

---
+++ app/src/main/java/com/github/mrvilkaman/namegenerator/SimpleActivity.java
@@ -0,0 +1,12 @@
+package com.github.mrvilkaman.namegenerator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SimpleActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
